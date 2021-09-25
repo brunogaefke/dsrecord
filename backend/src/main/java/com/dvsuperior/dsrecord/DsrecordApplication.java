8b48fc94d25895d9588dd92ea234dc19cd09cda8
Bruno Gaefke <brunogaefke@gmail.com>
2021-09-25 08:53:45 -0300

    backend project created

---
+++ backend/src/main/java/com/dvsuperior/dsrecord/DsrecordApplication.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsrecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsrecordApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dvsuperior/dsrecord/DsrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsrecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
