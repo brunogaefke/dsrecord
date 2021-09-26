408c9ed55ac91ef957c5f4838c49112f4ca93e50
Bruno Gaefke <brunogaefke@gmail.com>
2021-09-26 16:26:30 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_client (name) VALUES ('Bruno Gaefke');
+INSERT INTO tb_client (name) VALUES ('Bruno Gaefke');
+INSERT INTO tb_client (name) VALUES ('Bruno Gaefke');
+INSERT INTO tb_client (name) VALUES ('Bruno Gaefke');
+INSERT INTO tb_client (name) VALUES ('Bruno Gaefke');
