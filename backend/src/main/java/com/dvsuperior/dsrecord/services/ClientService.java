eb5b569fa8190fa8534fdb56c2ae9af50d53406d
Bruno Gaefke <brunogaefke@gmail.com>
2021-09-27 11:06:22 -0300

    Find Category by id, Exception Handling

--- backend/src/main/java/com/dvsuperior/dsrecord/services/ClientService.java
+++ backend/src/main/java/com/dvsuperior/dsrecord/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.dvsuperior.dsrecord.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dvsuperior.dsrecord.dto.ClientDTO;
 import com.dvsuperior.dsrecord.entities.Client;
 import com.dvsuperior.dsrecord.repositories.ClientRepository;
+import com.dvsuperior.dsrecord.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +23,12 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
